8235cb3d5aecfe6a1ae186d4a007ba9eeb9ce21e
pyqpyqpyq <alwayswannaflyx@gmail.com>
2020-09-05 23:41:29 +0800

    [pyqpyqpyq] feat:js_exercise_1.

--- __tests__/pieceOfCake/array_spec.js
+++ __tests__/pieceOfCake/array_spec.js
@@ -1,10 +1,9 @@
 describe('for array', () => {
   it('should get element of an array', () => {
     const array = [1, 2, 3, 4, 5];
-
     // <--start
     // Please write down the correct result. You should write the result directly.
-    const expected = undefined;
+    const expected = 3;
     // --end->
 
     expect(array[2]).toEqual(expected);
@@ -15,7 +14,7 @@ describe('for array', () => {
 
     // <--start
     // Please write one line of code to push some elements in the array to pass the test
-
+    array.push(6, 7, 8);
     // --end->
 
     expect(array).toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
@@ -24,10 +23,9 @@ describe('for array', () => {
   it('should be able to spread to array elements', () => {
     const array = [1, 2, 3, 4, 5];
     const newArray = [9, ...array, 10];
-
     // <--start
     // Please write down the correct value. You should write the final result directly.
-    const expected = undefined;
+    const expected = [9, 1, 2, 3, 4, 5, 10];
     // --end->
 
     expect(newArray).toEqual(expected);
@@ -38,8 +36,8 @@ describe('for array', () => {
 
     // <--start
     // Please write down the correct value. You should write the final result directly.
-    const expectedRow = undefined;
-    const expectedColumn = undefined;
+    const expectedRow = 2;
+    const expectedColumn = 3;
     // --end->
 
     expect(row).toEqual(expectedRow);
@@ -52,7 +50,7 @@ describe('for array', () => {
 
     // <--start
     // Please write down the correct value. You should write the final result directly.
-    const expected = undefined;
+    const expected = [2, 4];
     // --end->
 
     expect(filtered).toEqual(expected);
@@ -61,10 +59,10 @@ describe('for array', () => {
   it('should map array element to other value', () => {
     const numbers = [1, 2, 3, 4, 5];
     const mapped = numbers.map((n, i) => `Unit ${n} for element at index ${i}`);
-
+    // console.log('Debug:', mapped);
     // <--start
     // Please write down the correct value. You should write the final result directly.
-    const expected = undefined;
+    const expected = ['Unit 1 for element at index 0', 'Unit 2 for element at index 1', 'Unit 3 for element at index 2', 'Unit 4 for element at index 3', 'Unit 5 for element at index 4'];
     // --end->
 
     expect(mapped).toEqual(expected);
@@ -76,7 +74,7 @@ describe('for array', () => {
 
     // <--start
     // Please write down the correct value. You should write the final result directly.
-    const expected = undefined;
+    const expected = 15;
     // --end->
 
     expect(reduced).toEqual(expected);

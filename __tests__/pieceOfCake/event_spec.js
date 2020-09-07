cae8e9b3aaf8d13ab59b46ca5f627b880e4abf2f
pyqpyqpyq <alwayswannaflyx@gmail.com>
2020-09-07 20:46:53 +0800

    [pyqpyqpyq] feat:javascript-basic_event

--- __tests__/pieceOfCake/event_spec.js
+++ __tests__/pieceOfCake/event_spec.js
@@ -16,7 +16,9 @@ describe('for event', () => {
 
     // <--start
     // Please add the event listener to handle `click` event on `element`.
-
+    element.addListener('click', () => {
+      onClick();
+    });
     // --end->
 
     element.emit('click');
@@ -34,7 +36,7 @@ describe('for event', () => {
       .then(() => {
         // <--start
         // Please write down the correct value. You should write the final result directly.
-        const expected = undefined;
+        const expected = ['I have been clicked', 'I have been clicked'];
         // --end->
 
         expect(logs).toEqual(expected);
@@ -57,7 +59,7 @@ describe('for event', () => {
       .then(() => {
         // <--start
         // Please write down the correct value. You should write the final result directly.
-        const expected = undefined;
+        const expected = ['I have been clicked'];
         // --end->
 
         expect(logs).toEqual(expected);
